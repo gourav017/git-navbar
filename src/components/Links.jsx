97553a192990fb16283e951bab9f1a48e59f1413
gourav017 <gouravsahu696@gmail.com>
2022-05-18 20:45:12 +0530

    navbar deploy

---
+++ src/components/Links.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Links = () => {
+  return (
+    <div >
+        <a className='App-link' href="#">Services</a>
+        <a className='App-link' href="#">Project</a>
+        <a className='App-link' href="#">About</a>
+    </div>
+  )
+}
+
+export default Links
---
+++ src/components/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Logo = () => {
+  return (
+    <div className='App-logo'>LOGOBAKERY</div>
+  )
+}
+
+export default Logo
